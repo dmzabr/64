75db8692464df8cd2b5aa345794363dde8f5bedb
dmzabr <119075451+dmzabr@users.noreply.github.com>
2023-01-14 20:00:40 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число N. Программа выведет все натйральные числа от N до 0");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Function(N);
+
+void Function(int n)
+{
+    Console.Write($"{n} ");
+    if (n == 1)
+        return;
+    Function(n - 1);
+}
+
+Console.Read();
